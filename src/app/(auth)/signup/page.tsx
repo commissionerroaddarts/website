08c5be49d927c788c70bf52b3b9136b74690a936
Omer Khan <omerfarooqkhan7210@gmail.com>
2025-03-27 03:47:48 +0500

    created login and view listing page

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,12 +1,31 @@
 import React from "react";
 import SignupForm from "../../../components/authpages/SignupForm";
-import { generateMetadata } from "../../../utils/generateMetaData";
-
-export const metadata = generateMetadata({
+export const metadata = {
   title: "Sign Up - Road Darts",
   description: "Create an account to join the Road Darts community.",
-  url: "https://www.roaddarts.com/signup",
-});
+  icon: "/images/logos/road-darts-logo.png",
+  openGraph: {
+    title: "Sign Up - Road Darts",
+    description: "Create an account to join the Road Darts community.",
+    url: "https://www.roaddarts.com/signup",
+    type: "website",
+    images: [
+      {
+        url: "/images/logos/road-darts-logo.png",
+        width: 800,
+        height: 600,
+        alt: "Road Darts Logo",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@roaddarts",
+    title: "Sign Up - Road Darts",
+    description: "Create an account to join the Road Darts community.",
+    image: "/images/logos/road-darts-logo.png",
+  },
+};
 
 const page = () => {
   return <SignupForm />;
