21b484d68a65d15fafd980e7c551535816978016
Commissioner <commissioner@roaddarts.com>
2025-05-17 03:15:52 +0500

    fixed map issues and added info windows

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
           <Providers>
             <ToastContainer
               position="top-center"
-              autoClose={5000}
+              autoClose={2000}
               hideProgressBar={false}
               newestOnTop
               closeOnClick={false}
