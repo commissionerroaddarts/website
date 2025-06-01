ddf256a6128b4af764c5cd311ca3366b98e8fed1
Commissioner <commissioner@roaddarts.com>
2025-06-01 20:51:49 +0500

    added amenities in business

--- src/components/addestablishment/AddEstablishmentForm.tsx
+++ src/components/addestablishment/AddEstablishmentForm.tsx
@@ -92,6 +92,7 @@ export default function AddEstablishment({
             city: business?.location?.city ?? undefined,
             zipcode: business?.location?.zipcode ?? undefined,
             country: business?.location?.country ?? undefined,
+            address: business?.location?.address ?? undefined,
             geotag: {
               lat: business?.location?.geotag?.lat ?? undefined,
               lng: business?.location?.geotag?.lng ?? undefined,
